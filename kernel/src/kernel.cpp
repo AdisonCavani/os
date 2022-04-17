b3ce3e30888e5a99e31b9950875fbfd1f262cdc0
AdisonCavani <adriansrodon@tuta.io>
2022-04-17 15:46:59 +0200

    Add Page Frame Allocator

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,6 +1,7 @@
 
 #include "BasicRenderer.h"
 #include "Bitmap.h"
+#include "PageFrameAllocator.h"
 #include "cstr.h"
 #include "efiMemory.h"
 #include "memory.h"
@@ -14,51 +15,45 @@ struct BootInfo {
     uint64_t mMapDescSize;
 };
 
-uint8_t testbuffer[20];
+extern uint64_t _KernelStart;
+extern uint64_t _KernelEnd;
 
 extern "C" void
 _start(BootInfo *bootInfo) {
 
     BasicRenderer newRenderer = BasicRenderer(bootInfo->framebuffer, bootInfo->psf1_Font);
 
-    // Test displaying numbers with to_string method
-    newRenderer.CursorPosition = {1000, 0};
-    newRenderer.Print(to_string((int64_t)-1234976));
-    newRenderer.CursorPosition = {1000, 16};
-    newRenderer.Print(to_string((double)-13.14));
-
     uint64_t mMapEntries = bootInfo->mMapSize / bootInfo->mMapDescSize;
 
-    // List all memory descriptors with size
-    for (int i = 0; i < mMapEntries; i++) {
-        EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)((uint64_t)bootInfo->mMap + (i * bootInfo->mMapDescSize));
+    PageFrameAllocator newAllocator;
+    newAllocator.ReadEFIMemoryMap(bootInfo->mMap, bootInfo->mMapSize, bootInfo->mMapDescSize);
+
+    uint64_t kernelSize = (uint64_t)&_KernelEnd - (uint64_t)&_KernelStart;
+    uint64_t kernelPages = (uint64_t)kernelSize / 4096 + 1;
+
+    newAllocator.LockPages(&_KernelStart, kernelPages);
+
+    newRenderer.CursorPosition = {0, 0};
+    newRenderer.Print("Free RAM: ");
+    newRenderer.Print(to_string(newAllocator.GetFreeRAM() / 1024));
+    newRenderer.Print(" KB ");
+    newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
+
+    newRenderer.Print("Used RAM: ");
+    newRenderer.Print(to_string(newAllocator.GetUsedRAM() / 1024));
+    newRenderer.Print(" KB ");
+    newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
+
+    newRenderer.Print("Reserved RAM: ");
+    newRenderer.Print(to_string(newAllocator.GetReservedRAM() / 1024));
+    newRenderer.Print(" KB ");
+    newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
+
+    for (int t = 0; t < 20; t++) {
+        void *address = newAllocator.RequestPage();
+        newRenderer.Print(to_hstring((uint64_t)address));
         newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
-        newRenderer.Print(EFI_MEMORY_TYPE_STRINGS[desc->type]);
-        newRenderer.Colour = 0xffff00ff;
-        newRenderer.Print(" ");
-        newRenderer.Print(to_string(desc->numPages * 4096 / 1024));
-        newRenderer.Print(" KB");
-        newRenderer.Colour = 0xffffffff;
     }
-
-    Bitmap testBitmap;
-    testBitmap.Buffer = &testbuffer[0];
-    testBitmap.Set(0, false);
-    testBitmap.Set(1, true);
-    testBitmap.Set(2, false);
-    testBitmap.Set(3, false);
-    testBitmap.Set(4, true);
-    testBitmap.Set(16, true);
-
-    newRenderer.CursorPosition = {1400, 0};
-
-    for (int i = 0; i < 20; i++) {
-        newRenderer.CursorPosition = {1400, newRenderer.CursorPosition.Y + 16};
-        newRenderer.Print(testBitmap[i] ? "true" : "false");
-    }
-
-    newRenderer.CursorPosition = {1000, 500};
-    newRenderer.Print(to_string(GetMemorySize(bootInfo->mMap, mMapEntries, bootInfo->mMapDescSize)));
 
     return;
 }
