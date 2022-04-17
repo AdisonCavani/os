2fffe3e7de05f3775cd23d4b6d17ed4df57a5d55
AdisonCavani <adriansrodon@tuta.io>
2022-04-17 14:19:07 +0200

    Add Bitmap test

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,5 +1,6 @@
 
 #include "BasicRenderer.h"
+#include "Bitmap.h"
 #include "cstr.h"
 #include "efiMemory.h"
 #include "memory.h"
@@ -13,25 +14,22 @@ struct BootInfo {
     uint64_t mMapDescSize;
 };
 
-extern "C" void _start(BootInfo *bootInfo) {
+uint8_t testbuffer[20];
+
+extern "C" void
+_start(BootInfo *bootInfo) {
 
     BasicRenderer newRenderer = BasicRenderer(bootInfo->framebuffer, bootInfo->psf1_Font);
-    newRenderer.Print(to_string((uint64_t)1234976));
-    newRenderer.CursorPosition = {1000, 16};
+
+    // Test displaying numbers with to_string method
+    newRenderer.CursorPosition = {1000, 0};
     newRenderer.Print(to_string((int64_t)-1234976));
-    newRenderer.CursorPosition = {1000, 32};
+    newRenderer.CursorPosition = {1000, 16};
     newRenderer.Print(to_string((double)-13.14));
-    newRenderer.CursorPosition = {1000, 48};
-    newRenderer.Print(to_hstring((uint64_t)0xF0));
-    newRenderer.CursorPosition = {1000, newRenderer.CursorPosition.Y + 16};
-    newRenderer.Print(to_hstring((uint32_t)0xF0));
-    newRenderer.CursorPosition = {1000, newRenderer.CursorPosition.Y + 16};
-    newRenderer.Print(to_hstring((uint16_t)0xF0));
-    newRenderer.CursorPosition = {1000, newRenderer.CursorPosition.Y + 16};
-    newRenderer.Print(to_hstring((uint8_t)0xF0));
 
     uint64_t mMapEntries = bootInfo->mMapSize / bootInfo->mMapDescSize;
 
+    // List all memory descriptors with size
     for (int i = 0; i < mMapEntries; i++) {
         EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)((uint64_t)bootInfo->mMap + (i * bootInfo->mMapDescSize));
         newRenderer.CursorPosition = {0, newRenderer.CursorPosition.Y + 16};
@@ -41,6 +39,22 @@ extern "C" void _start(BootInfo *bootInfo) {
         newRenderer.Print(to_string(desc->numPages * 4096 / 1024));
         newRenderer.Print(" KB");
         newRenderer.Colour = 0xffffffff;
+    }
+
+    Bitmap testBitmap;
+    testBitmap.Buffer = &testbuffer[0];
+    testBitmap.Set(0, false);
+    testBitmap.Set(1, true);
+    testBitmap.Set(2, false);
+    testBitmap.Set(3, false);
+    testBitmap.Set(4, true);
+    testBitmap.Set(16, true);
+
+    newRenderer.CursorPosition = {1400, 0};
+
+    for (int i = 0; i < 20; i++) {
+        newRenderer.CursorPosition = {1400, newRenderer.CursorPosition.Y + 16};
+        newRenderer.Print(testBitmap[i] ? "true" : "false");
     }
 
     newRenderer.CursorPosition = {1000, 500};
